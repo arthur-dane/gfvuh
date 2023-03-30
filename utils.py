da81379e6e9999361b249405a73f0ac7e46e2b5c
Arthur Dayne <118467271+arthur-dane@users.noreply.github.com>
2023-03-30 11:17:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -437,17 +437,11 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
-async def tutorial_link(chat_id, link):
-    settings = await get_settings(chat_id) #fetching settings for group
-    if 'tutorial_video' in settings.keys():
-        TUTORIAL = settings['tutorial_video']
-    else:
-        TUTORIAL = TUTORIAL_VIDEO
-    https = link.split(":")[0]
-    if "http" == https:
-        https = "https"
-        link = link.replace("http", https)
-    return TUTORIAL
+async def add_tutorial_link(update, context):
+    chat_id = update.effective_chat.id
+    tutorial_link = context.args[0]
+    # You can save the tutorial link to a database or a file here
+    context.bot.send_message(chat_id=chat_id, text=f'Tutorial link added: {tutorial_link}')
 
 async def get_shortlink(chat_id, link):
     settings = await get_settings(chat_id) #fetching settings for group
