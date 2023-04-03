27d13e735968a7c9bd6d01d139c9739299e9ec73
Arthur Dayne <118467271+arthur-dane@users.noreply.github.com>
2023-04-03 16:01:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,6 @@
 import os, re
 from os import environ
 from Script import script
-from dotenv import load_dotenv
-load_dotenv()
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
